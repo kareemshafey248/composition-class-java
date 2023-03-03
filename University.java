903a0ea72ea1d51024471f8bd203fcbeda241617
kareem elshafey <113185738+kareemshafey248@users.noreply.github.com>
2023-03-03 08:37:16 +0200

    Add files via upload

---
+++ University.java
@@ -0,0 +1,35 @@
+public class University {
+   private departments;
+   
+   }
+
+   public void addDepartment {
+      departments.add;
+   }
+
+   // methods of University class
+}
+
+public class Department {
+   private String name;
+   
+   public Department(String name) {
+      this.name = name;
+      
+   }
+
+   public void addCourse(Course course) {
+      courses.add(course);
+   }
+}
+
+public class Course {
+   private String name;
+   private int credits;
+
+   public Course(String name, int credits) {
+      this.name = name;
+      this.credits = credits;
+   }
+
+}
